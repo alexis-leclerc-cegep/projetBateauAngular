01fa24154d3ef383454ae03e39b1419af63bda4d
= <=>
2021-11-14 13:54:03 -0500

    FONCTIONNEL

--- src/app/choix-modele/choix-modele.component.ts
+++ src/app/choix-modele/choix-modele.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import {Observable} from "rxjs";
+import { map } from "rxjs/operators"
+
+let tete = new HttpHeaders().set('access-control-allow-origin',"*");
+const BASE_PATH = "https://iwa2021.edriki.com/api/Boat/Search/";
 
 @Component({
   selector: 'app-choix-modele',
@@ -7,16 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoixModeleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+
   chercherBateau(event:any){
     let input = event.target.value;
+
     if (input.length >= 3){
-      console.log(event.target.value)
+      console.log(input);
+
+      console.log(`${BASE_PATH}${input}`);
+
+      this.http.get(`${BASE_PATH}${input}`)
+      .subscribe((data : any)=>{
+        console.log(data.response.datas);
+      })
+
     }
   }
+
+  getResult(search: string){
+    return this.http.get(`${BASE_PATH}${search}`, {headers: tete,responseType : 'json'});
+  }
+
 
 }
