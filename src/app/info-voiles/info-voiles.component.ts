0e9ff53bbb79f3d93a9afaf63b0424f0a07289a1
= <=>
2021-12-08 15:42:06 -0500

    travail le plaisir

--- src/app/info-voiles/info-voiles.component.ts
+++ src/app/info-voiles/info-voiles.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-info-voiles',
   templateUrl: './info-voiles.component.html',
   styleUrls: ['./info-voiles.component.css']
 })
+
+interface Voiles{
+  
+}
 export class InfoVoilesComponent implements OnInit {
 
   constructor() { }
 
+  afficher:boolean = false;
+  
+  @Input() Voiles : any;
+  result = {};
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    if (!changes['Voiles'].isFirstChange()){
+      this.result = changes['Voiles'].currentValue;
+      for (let i = 0; i < (this.result as any).length; i++){
+          if ((this.result as any)[i]['type'] == "GVE"){
+            console.log("oue");
+          }
+          switch((this.result as any)[i]['type']){
+            case "GVE":
+
+          } 
+      }
+      this.afficher = true;
+    }
+    console.log(this.result);
+  }
 }
